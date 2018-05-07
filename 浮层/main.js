f06dbc72e50009afb8a2d5f96c89073fc55d5858
sky <1815111899@qq.com>
2018-05-07 17:18:29 +0800

    浮层修改

--- 浮层/main.js
+++ 浮层/main.js
@@ -1,10 +1,13 @@
-clickMe.addEventListener('click', function(e){
-    popover.style.display = 'block'
-  })
-  wrapper.addEventListener('click', function(e){
-    e.stopPropagation()
-  })
-  
-  document.addEventListener('click', function(){
-    popover.style.display = 'none'
-  })
+$(clickMe).on('click',function () {
+    if($(popover).is(':hidden')){  //若浮层是关闭的，点击按钮则打开
+        $(popover).show();
+    }else {
+        $(popover).hide();  //和上面相反
+    }
+    $(document).one('click',function () {
+        $(popover).hide();
+    });
+});
+$(wrapper).on('click',function(e){
+    e.stopPropagation();  
+});
